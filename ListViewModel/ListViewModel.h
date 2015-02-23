e8b9b3750770e16940e4d4d5dcf18c54de81b81f
Adlai Holler <guy.on.internet@icloud.com>
2015-02-22 17:44:03 -0800

    Initial Commit

---
+++ ListViewModel/ListViewModel.h
@@ -0,0 +1,19 @@
+//
+//  ListViewModel.h
+//  ListViewModel
+//
+//  Created by Adlai Holler on 2/22/15.
+//  Copyright (c) 2015 Adlai Holler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ListViewModel.
+FOUNDATION_EXPORT double ListViewModelVersionNumber;
+
+//! Project version string for ListViewModel.
+FOUNDATION_EXPORT const unsigned char ListViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ListViewModel/PublicHeader.h>
+
+
